817ebdbe2fb96829a819c74a59d1fe8d392ee9de
Gijs Jorissen <mail@gijsjorissen.nl>
2020-01-22 15:31:46 +0100

    Adonis

--- aliases.zsh
+++ aliases.zsh
@@ -4,40 +4,45 @@ alias fcli="source $HOME/.zshrc"
 alias fdns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 
 # Short
-alias a='php artisan'
-alias c='composer'
-alias g='gulp'
-alias n='npm'
+alias a="php artisan"
+alias c="composer"
+alias g="gulp"
+alias n="npm"
 
 # Laravel
-alias amr='php artisan migrate:refresh'
-alias ams='php artisan migrate --seed'
-alias amrs='php artisan migrate:refresh --seed'
-alias amf='php artisan migrate:fresh'
-alias amfs='php artisan migrate:fresh --seed'
-alias arl='php artisan route:list'
-alias dusk='php artisan dusk'
+alias amr="php artisan migrate:refresh"
+alias ams="php artisan migrate --seed"
+alias amrs="php artisan migrate:refresh --seed"
+alias amf="php artisan migrate:fresh"
+alias amfs="php artisan migrate:fresh --seed"
+alias arl="php artisan route:list"
+alias dusk="php artisan dusk"
 
 # Composer
-alias cda='composer dump-autoload -o'
-alias ci='composer install --ignore-platform-reqs'
-alias cu='composer update --ignore-platform-reqs'
+alias cda="composer dump-autoload -o"
+alias ci="composer install --ignore-platform-reqs"
+alias cu="composer update --ignore-platform-reqs"
 alias cf="rm -rf vendor/ composer.lock && composer install --ignore-platform-reqs"
 
 # NPM
-alias ni='npm install'
-alias nrw='npm run watch'
-alias nrd='npm run dev'
-alias nrp='npm run production'
-alias nrs='npm run start'
-alias nrsd='npm run start:dev'
+alias ni="npm install"
+alias nrw="npm run watch"
+alias nrd="npm run dev"
+alias nrp="npm run production"
+alias nrs="npm run start"
+alias nrsd="npm run start:dev"
 alias nf="rm -rf node_modules/ package-lock.json && npm install"
 
+# Adonis
+alias ad="adonis"
+alias ads="adonis serve"
+alias adsd="adonis serve --dev"
+
 # Vagrant
-alias vu='vagrant up'
+alias vu="vagrant up"
 
 # PHPUnit
-alias pu='./vendor/bin/phpunit'
+alias pu="./vendor/bin/phpunit"
 
 # Weather
 weather() { curl -4 wttr.in/${1:-steenwijk} }
