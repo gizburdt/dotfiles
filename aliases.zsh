c3701f2951dcda1a88c27b19c0ca71c4c1660769
Gijs Jorissen <mail@gijsjorissen.nl>
2020-09-17 20:02:42 +0200

    opendb

--- aliases.zsh
+++ aliases.zsh
@@ -55,6 +55,23 @@ weather() { curl -4 wttr.in/${1:-steenwijk} }
 # Code
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
 
+# DB
+opendb () {
+    [ ! -f .env ] && { echo "No .env file found."; exit 1; }
+
+    DB_CONNECTION=$(grep ^DB_CONNECTION .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+    DB_HOST=$(grep ^DB_HOST .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+    DB_PORT=$(grep ^DB_PORT .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+    DB_DATABASE=$(grep ^DB_DATABASE .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+    DB_USERNAME=$(grep ^DB_USERNAME .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+    DB_PASSWORD=$(grep ^DB_PASSWORD .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+
+    DB_URL="${DB_CONNECTION}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+
+    echo "Opening ${DB_URL}"
+    open $DB_URL
+}
+
 # PHP
 phpv() {
     valet stop
