3331ff2b14118036c6bcbecb258209d57c89206e
Gijs Jorissen <mail@gijsjorissen.nl>
2021-01-06 16:46:42 +0100

    php

--- aliases.zsh
+++ aliases.zsh
@@ -78,7 +78,7 @@ opendb () {
 # PHP
 phpv() {
     valet stop
-    brew unlink php@7.2 php@7.3 php@7.4 php
+    brew unlink php@7.4 php
     brew link --force --overwrite $1
     brew services start $1
     composer global update
@@ -86,8 +86,6 @@ phpv() {
     valet install
 }
 
-alias php72="phpv php@7.2"
-alias php73="phpv php@7.3"
 alias php74="phpv php@7.4"
 alias php74="phpv php"
 
