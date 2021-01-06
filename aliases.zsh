3ed9a9b8b9f7e9c2ae9f6dafc041e2feebe48057
Gijs Jorissen <mail@gijsjorissen.nl>
2021-01-06 16:44:37 +0100

    php version

--- aliases.zsh
+++ aliases.zsh
@@ -78,7 +78,7 @@ opendb () {
 # PHP
 phpv() {
     valet stop
-    brew unlink php@5.6 php@7.2 php@7.3
+    brew unlink php@7.2 php@7.3 php@7.4 php
     brew link --force --overwrite $1
     brew services start $1
     composer global update
