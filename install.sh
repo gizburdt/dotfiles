16599ac4cd2e0b5514c34c807f1338405f9b596b
Gijs Jorissen <gizburdt@users.noreply.github.com>
2024-03-15 13:43:44 +0100

    wip

--- install.sh
+++ install.sh
@@ -15,12 +15,22 @@ sudo -v
 echo "Setting up this Mac..."
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if test ! $(which omz); then
+  	/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
  	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+ 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
@@ -33,7 +43,7 @@ brew bundle --file=$HOME/.dotfiles/install/Brewfile --verbose
 chsh -s $(which zsh)
 
 # Install global Composer packages
-/usr/local/bin/composer global require statamic/cli spatie/visit
+/opt/homebrew/bin/composer global require laravel/installer laravel-shift/cli statamic/cli
 
 # Install global NPM packages
 npm install -g aicommits
@@ -50,10 +60,6 @@ git config --global core.excludesfile $HOME/.dotfiles/.gitignore_global
 # Create directories
 mkdir $HOME/Server
 mkdir $HOME/System
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
