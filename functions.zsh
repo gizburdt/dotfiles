08e079ab2e96410978896edc8db587fc404118eb
Gijs Jorissen <mail@gijsjorissen.nl>
2024-06-04 15:10:50 +0200

    wip

--- functions.zsh
+++ functions.zsh
@@ -15,7 +15,7 @@ commit() {
 # Code
 storm() { open -a /Applications/PhpStorm.app --args $* }
 strom() { storm --args $* }
-code() { storm --args $* }
+code() { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
 
 # Weather
 weather() { curl -4 wttr.in/${1:-steenwijk} }
