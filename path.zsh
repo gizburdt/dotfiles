b6d9422d80c5b1f68a0796c1395a2ba558835be2
Gijs Jorissen <mail@gijsjorissen.nl>
2026-01-22 14:49:25 +0100

    wip

--- path.zsh
+++ path.zsh
@@ -6,3 +6,6 @@ export PATH="$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+# Local
+export PATH="$HOME/.local/bin:$PATH"
