b4b7df9ef1aab83de404b3789cad487807a2b72d
Gijs Jorissen <mail@gijsjorissen.nl>
2026-01-26 23:56:22 +0100

    wip

--- home/functions.zsh
+++ home/functions.zsh
@@ -1,17 +1,9 @@
-# Commit
-commit() {
-    commitMessage="$*"
-    git add .
-    eval "git commit -a -m '${commitMessage}'"
-}
-
 # IDE
 storm() { open -a /Applications/PhpStorm.app --args $* }
 strom() { storm --args $* }
 code() { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
 
-# Weather
-weather() { curl -4 wttr.in/${1:-steenwijk} }
-
-# QR
-qr() { curl qrcode.show/$1 }
+# Commit
+commit() {
+    eval "git add . && git commit -a -m '$*'"
+}
